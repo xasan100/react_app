c20f60b6e76a571ec97f1a107120e09139ad51bc
Abdurazakov xasan <102683894+Xasano5o@users.noreply.github.com>
2022-07-23 14:33:16 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,40 @@
 import React, { Component } from 'react'
-import Test from './test';
 
 export default class App extends Component {
-constructor(props){
-super(props)
-this.state={
-number1: '',
-  number2: '',
-  selected: ''  ,
-  resalt: ''
-}
-}
-  render() 
-  {
-    const {number1, number2,selected,resalt,}=this.state;
-   
+    constructor(props){
+        super(props) 
+        this.state={
+            addTextbox: [{}]
+        }
+    }
+    addControls (){
+        this.setState((
+            {
+                addTextbox:[... this.state.addTextbox,{}]
+            }
+        ))
+    }
+
+    delControls(i){
+        this.state.addTextbox.splice(i,1)
+        this.setState({})
+    }
+  render() {
     return (
       <div>
-        <br />
-        <input type="text" onChange={(e) => this.setState({ number1: e.target.value }) }/> 
-        <select name="" id=""
-          onChange={(e) => this.setState({    selected: e.target.value })}>
-          <option selected value="+">+</option>
-          <option value="-">-</option>
-          <option value="*">*</option>
-          <option value="/">/</option>
-        </select>
-        <input type="text" onChange={(e) => this.setState({ number2: e.target.value })} />
-        <button onClick={() => (this.setState({ resalt: 
-        (selected === '+' && +number1 + +number2 )||
-        ( selected === '-' && -number1 - +number2) ||
-        (selected === '*' && +number1 * +number2) ||
-        (selected === '/' && +number1 / +number2) 
-      }))}>=</button>
-        <h1>javobi { resalt}</h1>
-    <Test/>
-      </div>
+
+            <center> <div>Name:
+                {this.state.addTextbox.map((index) => (<div> <input type="text" placeholder=' Enter Name' />
+                {
+                    index ?
+                    <button onClick={()=>this.delControls(index)}><strong>❌</strong></button>
+                : null   
+                }
+                    </div> ))}  
+                  
+                   <h1></h1> </div>
+                <button onClick={() => this.addControls()}  >  add Constrols</button>
+       </center> </div>
     )
   }
 }
